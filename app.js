40b5bffee914b0563606e8f63d46c34c925f65e7
Janiek Colpaert <janiek.colpaert@gmail.com>
2025-02-03 19:29:32 +0100

    Update application

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const {Strategy} = require("passport-http-header-strategy");
 
 const questions = require("./routes/questions");
 
-const app = express();
 
+const app = express();
+app.use(passport.initialize());
+passport.use(new Strategy({ header: "X-API-KEY", param: "app_token", passReqToCallback: true },
+    function (req, token, done) {
+        console.log(token, done);
+        if( token === "blablabla"){
+            done();
+        }
+    }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
--- routes/questions.js
+++ routes/questions.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const { getQuestion } = require("../db/data");
+const passport = require("passport");
 
 const { DatabaseSync } = require("node:sqlite");
 const db = new DatabaseSync("./db/questions.sqlite");
 
 /* GET questions listing. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   const filter = req.query.tag;
-  
+
   if (filter) {
     const query = db.prepare("SELECT * FROM questions WHERE tags LIKE '%, ' || ? || ', %' OR tags LIKE ? || ', %' OR tags LIKE '%, ' || ? OR tags = ?");
     return res.json(query.all(filter, filter, filter, filter));
-  } 
+  }
 
   const query = db.prepare("SELECT * FROM questions");
   res.json(query.all());
 });
 
 /* GET a single question by id. */
-router.get("/:id", function(req, res, next) {
+router.get("/:id", function (req, res, next) {
   const id = req.params.id;
   const question = getQuestion(id);
 
   if (!question) {
     return res.status(404).json({ error: "Question not found" });
   }
-  else{
+  else {
     const update = db.prepare("update questions set view_count = view_count + 1 where id = ?");
     update.run(id);
     question.view_count = question.view_count + 1;
@@ -36,30 +37,33 @@ router.get("/:id", function(req, res, next) {
 });
 
 /* CREATE a new question. */
-router.post("/", function(req, res, next) {
-  const { tags, title } = req.body;
+router.post("/", 
+  function (req, res, next) {
+    const { tags, title } = req.body;
 
-  if (!tags ||  !title) {
-    return res.status(400).json({ error: "All fields are required - Your fault" });
-  }
+    if (!tags || !title) {
+      res.status(400).json({ error: "All fields are required - Your fault" });
+      return;
+    }
 
-  const query = db.prepare("INSERT INTO questions (tags, title) VALUES (?, ?, ?, ?, ?)");
-  const result = query.run(tags, 0, new Date().toISOString(), title, false.toString());
+    const query = db.prepare("INSERT INTO questions (tags, view_count, creation_date, title, is_answered) VALUES (?, ?, ?, ?, ?)");
+    const result = query.run(tags, 0, new Date().toISOString(), title, false.toString());
 
-  if(result.lastInsertRowid) {
-    res.status(201).json(getQuestion(result.lastInsertRowid));
-  }
-  res.status(500).json({error: "Something wrong happened on our side - My Fault"});
+    if (result.lastInsertRowid) {
+      res.status(201).json(getQuestion(result.lastInsertRowid));
+      return;
+    }
+    res.status(500).json({ error: "Something wrong happened on our side - My Fault" });
 
-});
+  });
 
 /* UPDATE a question by id. */
-router.put("/:id", function(req, res, next) {
+router.put("/:id", function (req, res, next) {
   const id = req.params.id;
   const { tags, is_answered, title } = req.body;
 
   if (!tags || !is_answered || !title) {
-    if(is_answered === false) {
+    if (is_answered === false) {
       return res.status(400).json({ error: "Please pass false as a string - Your fault RTFM" });
     }
     return res.status(400).json({ error: "All fields are required - Your fault" });
@@ -76,7 +80,7 @@ router.put("/:id", function(req, res, next) {
 });
 
 /* PATCH (partial update) a question by id. */
-router.patch("/:id", function(req, res, next) {
+router.patch("/:id", function (req, res, next) {
   const id = req.params.id;
   const fields = req.body;
   const queryParts = [];
@@ -92,18 +96,18 @@ router.patch("/:id", function(req, res, next) {
   }
 
   values.push(id);
-  const query = db.prepare(`UPDATE questions SET ${queryParts.join(', ')} WHERE id = ?`);
+  const query = db.prepare(`UPDATE questions SET ${queryParts.join(", ")} WHERE id = ?`);
   const result = query.run(...values);
 
   if (result.changes === 0) {
-    return res.status(404).json({error: "Question not found - Your fault"});
+    return res.status(404).json({ error: "Question not found - Your fault" });
   }
 
   res.json(getQuestion(id));
 });
 
 /* DELETE a question by id. */
-router.delete("/:id", function(req, res, next) {
+router.delete("/:id", function (req, res, next) {
   const id = req.params.id;
   const query = db.prepare("DELETE FROM questions WHERE id = ?");
   const result = query.run(id);
