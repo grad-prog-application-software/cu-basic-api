0c1ae035357242b1f0795f2d4a09612cb9cf90d3
Janiek Colpaert <janiek.colpaert@gmail.com>
2025-02-11 12:44:39 +0100

    events fix

--- db/data-events.js
+++ db/data-events.js
@@ -2,7 +2,7 @@ const { DatabaseSync } = require("node:sqlite");
 const db = new DatabaseSync("./db/events.sqlite");
 
 function getEvent(id) {
-    const query = db.prepare("SELECT * FROM events WHERE id = ?");
+    const query = db.prepare("SELECT * FROM events WHERE event_id = ?");
     return query.get(id);
 }
 
